66220865c477e338e6654bb03151e1b9c8244a22
Elias Lezcano <elias4k@live.com>
2018-06-21 20:28:50 -0300

    hhhjkl

--- Test/Test/Startup.cs
+++ Test/Test/Startup.cs
@@ -27,7 +27,7 @@ namespace Test
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Algo asd");
             });
         }
     }
